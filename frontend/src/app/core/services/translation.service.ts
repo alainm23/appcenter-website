6143d385f4fed86507483355dffde27f642d4bc3
Alain <alainmh23@gmail.com>
2024-12-12 11:11:20 -0500

    add translate module

--- frontend/src/app/core/services/translation.service.ts
+++ frontend/src/app/core/services/translation.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Inject, PLATFORM_ID } from '@angular/core';
+import { Injectable, Inject, PLATFORM_ID, LOCALE_ID } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { isPlatformBrowser } from '@angular/common';
 
@@ -7,6 +7,7 @@ import { isPlatformBrowser } from '@angular/common';
 })
 export class TranslationService {
   defaultLang = 'en';
+  supportedLangs = ['en', 'es'];
 
   constructor(
     private translateService: TranslateService,
@@ -14,15 +15,46 @@ export class TranslationService {
   ) {
     if (isPlatformBrowser(this.platformId)) {
       const savedLang = localStorage.getItem('lng');
-      if (savedLang) {
-        this.defaultLang = savedLang;
-      }
+      const userLang = this.getUsersLocale();
+
+      this.defaultLang = savedLang
+        ? savedLang
+        : this.supportedLangs.includes(userLang)
+        ? userLang
+        : this.defaultLang;
+
       this.translateService.setDefaultLang(this.defaultLang);
       this.translateService.use(this.defaultLang);
     }
   }
 
+  getUsersLocale(defaultValue: string = this.defaultLang): string {
+    if (typeof window === 'undefined' || !window.navigator) {
+      return defaultValue;
+    }
+
+    const navigator = window.navigator as Navigator & {
+      userLanguage?: string;
+      browserLanguage?: string;
+    };
+    let lang = navigator.languages
+      ? navigator.languages[0]
+      : navigator.language;
+
+    lang =
+      lang ||
+      navigator.browserLanguage ||
+      navigator.userLanguage ||
+      defaultValue;
+
+    return lang.split('-')[0];
+  }
+
   changeLang(lang: string) {
+    if (!this.supportedLangs.includes(lang)) {
+      console.warn(`Idioma no soportado: ${lang}`);
+      return;
+    }
     this.translateService.use(lang);
     if (isPlatformBrowser(this.platformId)) {
       localStorage.setItem('lng', lang);
